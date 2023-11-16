2800ea67d3fb8087d40f1285a557c264a16616db
Candi <Alcantarilla.cacan24@triana.salesianos.edu>
2023-11-16 10:44:32 +0100

    División entera terminada

--- Incrediblecalculator.java
+++ Incrediblecalculator.java
@@ -35,7 +35,11 @@ public class IncredibleCalculator {
 
                     System.out.println("Solución: " + multiplicacion(num1 * num2));
                     break;
-            case 4: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división entera.
+            case 4:
+                    int num1=leerEnteroEntrada;
+                    int nm2=leerEnteroEntrada;
+
+                    System.out.println("Solución: " + divisionEntera(num1 / num2));
                     break;
             case 5: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división con decimales.
                     break;
